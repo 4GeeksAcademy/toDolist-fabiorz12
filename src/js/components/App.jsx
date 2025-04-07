6a2f86278ff8d2ca373f6789da0ed9dae67466d5
Fabio Rodríguez <fabioaro10@gmail.com>
2025-04-07 20:25:48 +0000

    to do list con estilos y boton eliminar

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -21,18 +21,35 @@ function App() {
         }
     }
 
+    const deleteTask = (id) => {
+        const updatedTasks = task.filter((task) => task.id !== id);
+        setTask(updatedTasks);
+    }
+
     return (
         <>
             <div className="todo-container">
                 <h2>Task Manager</h2>
                 <div className="todo-input">
-                    <input type="text" placeholder="Add a new task" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+                    <input 
+                    type="text" 
+                    placeholder="Add a new task" 
+                    value={newTask} 
+                    onChange={(e) => setNewTask(e.target.value)} 
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            addTask();
+                        }
+                    }} 
+                    />
                     <button onClick={addTask} className="btn btn-primary">Add</button>
                 </div>
+                {task.length === 0 && <p>No hay tareas añadir tareas</p>}
                 <ul>
                     {task.map((task) => (
                         <li key={task.id}>
                             <span>{task.text}</span>
+                            <button onClick={() => deleteTask(task.id)} className="delete-btn btn btn-danger">X</button>
                         </li>
 
                     ))}
